bb20cd55c371a5b3d731b1118223b1d7912c1d7e
ktakano <takano@ic.kanagawa-it.ac.jp>
2026-04-12 11:29:05 +0900

    Update app.py
    - 画像を保存できるようにした。(/predict)
    - PNGを表示機能を追加した。(/returnPNG)

--- app.py
+++ app.py
@@ -8,27 +8,47 @@ from PIL import Image
 import keras
 from keras.models import load_model
 from keras.preprocessing.image import img_to_array
+import json
+import base64
 
 app = Flask(__name__)
 
 # モデルの読み込み
 try:
-    # 2.15環境で古いモデルを読み込む際の互換性フラグ
-    model = load_model('model_mini.keras', compile=False)
+    model = load_model('model/model_large.keras', compile=False)
     print("--- モデルの読み込みに成功しました ---")
 except Exception as e:
     print(f"読み込み失敗: {e}")
 
+#モデルのラベル設定
 categories = ['30', '60', '100']
 
-@app.route('/', methods=['POST'])
-def top():
-    image_bytes = request.data
-    if not image_bytes:
-        return jsonify({"error": "No data received"}), 400
+#画像枚数カウンタの設定
+COUNTER_FILE = "count.txt"
+
+
+#画像枚数カウンタ用_カウントアップ関数
+def get_next_count():
+    if os.path.exists(COUNTER_FILE):
+        with open(COUNTER_FILE, "r") as f:
+            return int(f.read().strip())
+    return 0
+
+#画像枚数カウンタ用_カウント保存関数
+def save_count(count):
+    with open(COUNTER_FILE, "w") as f:
+        f.write(str(count))
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    current_count = get_next_count() + 1
+    image = request.get_data()
+
+    if len(image) == 0:
+        return "サーバーには何も届いていません(0 bytes)", 400
     
     try:
-        img = Image.open(io.BytesIO(image_bytes))
+        img = Image.open(io.BytesIO(image))
         img = img.convert('RGB')
         img = img.resize((150, 150))
         
@@ -38,11 +58,37 @@ def top():
         predictions = model.predict(img_expand)
         predicted_class_index = np.argmax(predictions[0])
         predicted_label = categories[predicted_class_index]
+
+        #画像を保存
+        # バイナリデータを画像オブジェクトに変換
+        img = Image.open(io.BytesIO(image))
         
-        return jsonify({"result": predicted_label})
+        # PNGとして保存
+        name = f"./picture/image_{current_count}.png"
+        img.save(name, "PNG")
+        
+        print("PNG形式で保存しました。")
+        
+        return jsonify({"image_path" : f"./picture/image_{current_count}.png", "predicted_label": predicted_label}),200
     
     except Exception as e:
-        return jsonify({"ok": False, "error": str(e)}), 500
+        return "error"+str(e), 500
+
+@app.route('/returnPNG', methods=['POST'])
+def returnPNG():
+    file_path = request.get_data()
+    
+    with open(file_path, "rb") as image_file:
+        # 1. 画像をバイナリで読み込む
+        binary_data = image_file.read()
+        
+        # 2. Base64でエンコードし、さらに文字列(utf-8)に変換
+        base64_string = base64.b64encode(binary_data).decode('utf-8')
+    
+    # 3. 辞書形式にして返す（Flask等ならそのままreturn可能）
+    return {
+        "image_data": base64_string
+    }
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=False)
